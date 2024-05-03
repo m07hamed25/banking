7fce2e47c9d89c92636437cda2df2d3eb8525b64
Mohamed Reda <129080774+m07hamed25@users.noreply.github.com>
2024-05-03 15:22:26 +0300

    Sidebar

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,28 @@
 import React from 'react'
+import HeaderBox from '@/components/HeaderBox'
+import TotalBalanceBox from '@/components/TotalBalanceBox'
 
 const Home = () => {
+  const loggedIn = {firsName: 'Mohamed'}
+
   return (
-    <div>
-      Home 
+    <div className='home'>
+      <div className='home-content'>
+        <header className='home-header'>
+          <HeaderBox 
+           type='greeting'
+           title='Welcome'
+           user={loggedIn?.firsName || 'Guest'}
+           subtext='Access and manage your account and transaction efficiently.'
+          />
+
+          <TotalBalanceBox 
+           accounts={[]}
+           totalBanks={1}
+           totalCurrentBalance={1250.35}
+          />
+        </header>
+      </div>
     </div>
   )
 }
